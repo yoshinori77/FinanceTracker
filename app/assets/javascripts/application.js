fee9b1e928cc40a512f67809b2dcd0ebeb93b1d9
yamashitayoshinori <yoshinoriyamashita123@gmail.com>
2016-11-20 05:16:17 +0000

    Add user and stock association

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require turbolinks
-//= require ./stocks
+// require turbolinks
 //= require_tree .
 
 var hide_spinner = function() {
