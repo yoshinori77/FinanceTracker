6ebf8fa7feb5482f2894a0583f21853393938d9d
yamashitayoshinori <yoshinoriyamashita123@gmail.com>
2016-11-21 13:24:11 +0000

    Add destroy method and show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "user/registrations" }
   resources :user_stocks, except: [:show, :edit, :update]
-  resources :user, only: [:show]
+  resources :users, only: [:show]
   resources :friendships
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
